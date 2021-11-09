4cf1274ff8f682d53db8412d9cc0225432bb39f1
xcript1 <o_olatunji@mail.harper.edu>
2021-11-09 15:18:46 -0600

    Added images to show page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
 const Campground = require('./models/campground');
 
@@ -20,6 +21,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.engine('ejs', ejsMate)
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
