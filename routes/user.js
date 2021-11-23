a829df1c3424c288f70c55c87eb5f0c68c4aa4c1
David <vodolatunji@gmail.com>
2021-11-23 11:08:12 -0600

    Authentication and Authorizatio of users for reviews and campground done. Should refector code soon.

--- routes/user.js
+++ routes/user.js
@@ -15,9 +15,11 @@ router.post('/register', catchAsync(async (req, res) => {
         const { email, username, password } = req.body
         const user = new User({ email, username })
         const registeredUser = await User.register(user, password)
-        console.log(registeredUser)
-        req.flash('success', 'Welcome to Yelp Camp')
-        res.redirect('/campgrounds')
+        req.login(registeredUser, err => {
+            if (err) return next(err)
+            req.flash('success', 'Welcome to Yelp Camp')
+            res.redirect('/campgrounds')
+        })
     } catch (e) {
         req.flash('error', e.message)
         res.redirect('register')
@@ -31,7 +33,9 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back')
-    res.redirect('/campgrounds')
+    const redirectUrl = req.session.returnTo || '/campgrounds'
+    delete req.session.returnTo
+    res.redirect(redirectUrl)
 })
 
 router.get('/logout', (req, res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
+            author: '619bf794c76d7a0d13a9c0d4',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
