32719caaa2aa9a68092996eb7a64446d42032b42
Davidxcr <vodolatunji@gmail.com>
2023-01-02 00:23:43 -0600

    finished issue with env again

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+require('dotenv').config()
 const mongoose = require('mongoose')
 const cities = require('./cities')
 const { places, descriptors } = require('./seedHelpers')
